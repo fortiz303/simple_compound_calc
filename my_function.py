df8672df6ed53a62634e4a59f5d15ba2ce904dae
New <new@News-MacBook-Air.local>
2021-09-01 12:53:06 -0600

    Added a second feature to calculate days to reach amount wanted

--- my_function.py
+++ my_function.py
@@ -1,6 +1,16 @@
-days = int(input("days? "))
-amount = int(input("amount? "))
+#days = int(input("days? "))
+#amount = int(input("amount? "))
 
-for n in range(int(days)):
-	amount = amount + amount * .08
-	print(amount)
+#for n in range(int(days)):
+#	amount = amount + amount * .08
+#	print(amount)
+
+amount_wanted = int(input("amount wanted? "))
+
+amount = int(input("what is the amount that you will begin with? "))
+day = 0
+while amount_wanted > amount:
+	amount = amount + amount * 0.08
+	day += 1
+
+print("It will take you appx " f'{day}' "days")
